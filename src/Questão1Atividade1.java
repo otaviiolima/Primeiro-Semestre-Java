4a9efffd8bc474a9da7e00752edb4eeeb9c07c53
Luiz Otávio <otaviio.lima@hotmail.com>
2026-03-28 15:38:52 -0300

    Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.

---
+++ src/Questão1Atividade1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Questão1Atividade1 {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        // O usuário vai digitar os três números.
+
+        System.out.println("Digite o primeiro número: ");
+        int numA = scanner.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int numB = scanner.nextInt();
+
+        System.out.println("Digite o terceiro número: ");
+        int numC = scanner.nextInt();
+
+        // Aqui o programa vai somar o primeiro (numA) e segundo número (numB)
+        // e vai dizer se é maior ou menor que o terceiro número (numC).
+
+
+        if (numA + numB > numC) {
+            System.out.println("A soma entre os dois números é maior que C!");
+        } else if (numA + numB <= numC) {
+            System.out.println("A soma entre os dois números não é maior que C!");
+        }
+
+    }
+}
---
+++ src/Questão2Atividade1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Questão2Atividade1 {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        // O usuário vai informar sua idade.
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        // Aqui o programa vai informar se ele é maior ou menor de idade.
+
+        if (idade >= 18) {
+            System.out.println("Maior de Idade!");
+        } else if (idade < 18) {
+            System.out.println("Menor de Idade!");
+
+        }
+
+    }
+}
